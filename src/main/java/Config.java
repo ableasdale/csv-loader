55aebdfa4594f59d5ddc3438a63d7011c5f6db96
Alex Bleasdale <ableasdale@marklogic.com>
2016-05-26 20:55:19 +0100

    Config change

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -3,8 +3,8 @@
  */
 public class Config {
 
-    public static String BASE_DATA_DIR = "/Users/ableasdale/Documents/data/panama/offshore_leaks_csvs-20160523";
-    public static String MARKLOGIC_XCC_URI = "xcc://q:q@localhost:8000/Panama";
+    public static String BASE_DATA_DIR = "/Users/ableasdale/Documents/data/csvs";
+    public static String MARKLOGIC_XCC_URI = "xcc://q:q@localhost:8000/CSVs";
     public static Integer THREADPOOL_SIZE = 32;
 
 }
